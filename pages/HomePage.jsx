93e8af3a6b6b1bcc1ffbcf1af42c21780616fe95
sesmaniraj <sesmaniraj7@gmail.com>
2024-04-18 17:16:02 +0545

    refactor:menu bar updated for home page

---
+++ pages/HomePage.jsx
@@ -0,0 +1,203 @@
+import React, { useState, useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+  Animated,
+  PanResponder,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons"; // Import Ionicons from react-native-vector-icons
+import HomeButton from "../components/HomeButton";
+
+const { width, height } = Dimensions.get("window");
+
+const HomePage = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuAnimation = useRef(
+    new Animated.ValueXY({ x: -width, y: 0 })
+  ).current;
+
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event(
+        [
+          null,
+          {
+            dx: pan.x,
+            dy: pan.y,
+          },
+        ],
+        { useNativeDriver: false } // Add options here
+      ),
+      onPanResponderRelease: (e, gestureState) => {
+        if (gestureState.dx > width / 3) {
+          closeMenu();
+        } else {
+          Animated.spring(pan, {
+            toValue: { x: 0, y: 0 },
+            useNativeDriver: false,
+          }).start();
+        }
+      },
+    })
+  ).current;
+
+  const toggleMenu = () => {
+    if (isMenuOpen) {
+      closeMenu();
+    } else {
+      setIsMenuOpen(true);
+      Animated.timing(menuAnimation, {
+        toValue: { x: 0, y: 0 },
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  };
+
+  const closeMenu = () => {
+    Animated.timing(menuAnimation, {
+      toValue: { x: -width, y: 0 },
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => setIsMenuOpen(false));
+  };
+
+  const handleRefresh = () => {
+    // Add your refresh logic here
+    console.log("Refresh button pressed");
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.top_bar}>
+        <TouchableOpacity style={styles.menuToggle} onPress={toggleMenu}>
+          <Ionicons
+            name={isMenuOpen ? "" : "menu"}
+            size={width * 0.06}
+            color="white"
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.refreshIcon} onPress={handleRefresh}>
+          <Ionicons name="refresh" size={width * 0.06} color="white" />
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <Text style={styles.heading}>HomePage</Text>
+      </ScrollView>
+
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[
+          styles.menu,
+          {
+            transform: [{ translateX: menuAnimation.x }, { translateX: pan.x }],
+          },
+        ]}
+      >
+        <View>
+          <Text style={styles.menu_top}>Hello</Text>
+        </View>
+        <TouchableOpacity style={styles.menuItem}>
+          <Text style={styles.menuItemText}>Menu Item 1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <Text style={styles.menuItemText}>Menu Item 2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <Text style={styles.menuItemText}>Menu Item 3</Text>
+        </TouchableOpacity>
+      </Animated.View>
+      {isMenuOpen && (
+        <TouchableOpacity style={styles.overlay} onPress={closeMenu} />
+      )}
+      <HomeButton />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  menuToggle: {
+    position: "absolute",
+    top: 30,
+    left: 20,
+    zIndex: 1,
+  },
+  top_bar: {
+    backgroundColor: "#2cd380",
+    height: 300,
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+  },
+  refreshIcon: {
+    position: "absolute",
+    top: 30,
+    right: 20,
+    zIndex: 1,
+  },
+  heading: {
+    fontSize: width * 0.05,
+    marginBottom: 20,
+    paddingHorizontal: 20,
+  },
+  menu_top: {
+    height: 100,
+    backgroundColor: "#2cd380",
+    color: "white",
+    textAlign: "center",
+    justifyContent: "center",
+  },
+  menu: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    height: height, // Take up full height of screen
+    width: width * 0.5, // Adjust width as needed
+    backgroundColor: "#f8f9fa", // Adjust background color here
+    borderColor: "#ccc",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  menuItem: {
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 4,
+    backgroundColor: "#f8f9fa",
+  },
+  menuItemText: {
+    fontSize: width * 0.04,
+    color: "black",
+  },
+  scrollViewContent: {
+    paddingTop: height * 0.08, // Adjust the paddingTop to accommodate the icons
+    flexGrow: 1,
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: width,
+    height: height,
+    zIndex: 0,
+  },
+});
+
+export default HomePage;
---
+++ pages/LoginPage.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+const LoginPage = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigation = useNavigation();
+
+  const handleSendCode = () => {
+    // Remove any non-numeric characters from the phone number
+    const formattedPhoneNumber = phoneNumber.replace(/\D/g, "");
+
+    // Check if the formatted phone number contains only numeric characters
+    if (!/^\d+$/.test(formattedPhoneNumber)) {
+      setErrorMessage("Phone number must contain only numeric characters");
+      return;
+    }
+
+    // Check if the formatted phone number has 10 digits
+    if (formattedPhoneNumber.length !== 10) {
+      setErrorMessage("Phone number must be 10 digits long");
+      return;
+    }
+
+    // If no validation errors, proceed to navigate to the OTP page
+    setErrorMessage(""); // Clear the error message if no error
+    console.log("Sending code to:", formattedPhoneNumber);
+    navigation.navigate("Otp");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Enter Your Phone Number</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Phone Number"
+        keyboardType="phone-pad"
+        onChangeText={setPhoneNumber}
+        value={phoneNumber}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSendCode}>
+        <Text style={styles.buttonText}>Send Code</Text>
+      </TouchableOpacity>
+      {errorMessage ? <Text style={styles.error}>{errorMessage}</Text> : null}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "2cd380",
+    width: "100%",
+    height: 50,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  error: {
+    color: "red",
+    marginTop: 10,
+  },
+});
+
+export default LoginPage;
