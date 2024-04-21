74b75c216d241070533abf645434c1c8f72d3896
sesmaniraj <sesmaniraj7@gmail.com>
2024-04-21 11:24:44 +0545

    refactor: menu  bar code seperated from homepage

---
+++ components/Menu.tsx
@@ -0,0 +1,162 @@
+import React, { useRef, useEffect } from "react";
+import {
+  View,
+  Animated,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faChartBar,
+  faFileInvoiceDollar,
+  faWallet,
+  faMoneyCheckAlt,
+  faCalculator,
+  faFileAlt,
+  faBalanceScale,
+  faMoneyBill,
+  faReceipt,
+  faCreditCard,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Menu = ({ isMenuOpen, menuAnimation }) => {
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      openMenu();
+    } else {
+      closeMenu();
+    }
+  }, [isMenuOpen]);
+
+  const openMenu = () => {
+    Animated.timing(menuAnimation, {
+      toValue: { x: 0, y: 0 },
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const closeMenu = () => {
+    Animated.timing(menuAnimation, {
+      toValue: { x: -300, y: 0 }, // Adjust the value according to your menu width
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  return (
+    <Animated.View
+      ref={menuRef}
+      style={[
+        styles.menu,
+        {
+          transform: [{ translateX: menuAnimation.x }],
+        },
+      ]}
+    >
+      <Text style={styles.menu_top}>Welcome, Sajan</Text>
+      <ScrollView
+        contentContainerStyle={styles.scrollContainer}
+        showsVerticalScrollIndicator={false}
+      >
+        <TouchableOpacity style={styles.menuItem}>
+          <FontAwesomeIcon icon={faChartBar} style={styles.icon} />
+          <Text style={styles.menuItemText}>Financial Reports</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <FontAwesomeIcon icon={faFileInvoiceDollar} style={styles.icon} />
+          <Text style={styles.menuItemText}>Invoices</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <FontAwesomeIcon icon={faWallet} style={styles.icon} />
+          <Text style={styles.menuItemText}>Wallet</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <FontAwesomeIcon icon={faMoneyCheckAlt} style={styles.icon} />
+          <Text style={styles.menuItemText}>Payments</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <FontAwesomeIcon icon={faCalculator} style={styles.icon} />
+          <Text style={styles.menuItemText}>Calculator</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <FontAwesomeIcon icon={faFileAlt} style={styles.icon} />
+          <Text style={styles.menuItemText}>Documents</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <FontAwesomeIcon icon={faBalanceScale} style={styles.icon} />
+          <Text style={styles.menuItemText}>Balances</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <FontAwesomeIcon icon={faMoneyBill} style={styles.icon} />
+          <Text style={styles.menuItemText}>Bills</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <FontAwesomeIcon icon={faReceipt} style={styles.icon} />
+          <Text style={styles.menuItemText}>Receipts</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <FontAwesomeIcon icon={faCreditCard} style={styles.icon} />
+          <Text style={styles.menuItemText}>Credit Cards</Text>
+        </TouchableOpacity>
+        {/* Add more menu items here */}
+      </ScrollView>
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  menu: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: 300, // Adjust the width as needed
+    height: "100%",
+    backgroundColor: "#f8f9fa",
+    borderColor: "#ccc",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  menu_top: {
+    height: 90,
+    backgroundColor: "#2cd380",
+    color: "white",
+    textAlign: "center",
+    justifyContent: "center",
+    paddingTop: 30,
+    fontSize: 25,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    paddingBottom: 20, // Add some padding to the bottom to prevent content from being hidden behind the navigation bar
+  },
+  menuItem: {
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 4,
+    backgroundColor: "#f8f9fa",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  menuItemText: {
+    fontSize: 16,
+    color: "black",
+    marginLeft: 10,
+  },
+  icon: {
+    color: "#2cd380",
+    fontSize: 24,
+  },
+});
+
+export default Menu;
--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -8,12 +8,13 @@ import {
   Animated,
   PanResponder,
 } from "react-native";
-import { Ionicons } from "@expo/vector-icons"; // Import Ionicons from react-native-vector-icons
+import { Ionicons } from "@expo/vector-icons";
 import HomeButton from "../components/HomeButton";
 import HorizontalCarousel from "../components/HorizontalCarousel";
 import TabNavigator from "../components/TabNavigator";
 import PlusButton from "../components/PlusButton";
 import ImageSlider from "../components/ImageSlider";
+import Menu from "../components/Menu";
 
 const { width, height } = Dimensions.get("window");
 
@@ -23,45 +24,8 @@ const HomePage = () => {
     new Animated.ValueXY({ x: -width, y: 0 })
   ).current;
 
-  const pan = useRef(new Animated.ValueXY()).current;
-
-  const panResponder = useRef(
-    PanResponder.create({
-      onMoveShouldSetPanResponder: () => true,
-      onPanResponderMove: Animated.event(
-        [
-          null,
-          {
-            dx: pan.x,
-            dy: pan.y,
-          },
-        ],
-        { useNativeDriver: false } // Add options here
-      ),
-      onPanResponderRelease: (e, gestureState) => {
-        if (gestureState.dx > width / 3) {
-          closeMenu();
-        } else {
-          Animated.spring(pan, {
-            toValue: { x: 0, y: 0 },
-            useNativeDriver: false,
-          }).start();
-        }
-      },
-    })
-  ).current;
-
   const toggleMenu = () => {
-    if (isMenuOpen) {
-      closeMenu();
-    } else {
-      setIsMenuOpen(true);
-      Animated.timing(menuAnimation, {
-        toValue: { x: 0, y: 0 },
-        duration: 300,
-        useNativeDriver: true,
-      }).start();
-    }
+    setIsMenuOpen((prev) => !prev); // Toggle menu state
   };
 
   const closeMenu = () => {
@@ -73,7 +37,6 @@ const HomePage = () => {
   };
 
   const handleRefresh = () => {
-    // Add your refresh logic here
     console.log("Refresh button pressed");
   };
 
@@ -99,73 +62,7 @@ const HomePage = () => {
       <HorizontalCarousel />
       <TabNavigator />
 
-      <Animated.View
-        {...panResponder.panHandlers}
-        style={[
-          styles.menu,
-          {
-            transform: [{ translateX: menuAnimation.x }, { translateX: pan.x }],
-          },
-        ]}
-      >
-        <View>
-          <Text style={styles.menu_top}>Hello</Text>
-        </View>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 3</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 3</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 3</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 3</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 3</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Menu Item 3</Text>
-        </TouchableOpacity>
-      </Animated.View>
+      <Menu isMenuOpen={isMenuOpen} menuAnimation={menuAnimation} />
       {isMenuOpen && (
         <TouchableOpacity style={styles.overlay} onPress={closeMenu} />
       )}
@@ -198,6 +95,7 @@ const styles = StyleSheet.create({
     left: 70,
     zIndex: 1,
   },
+
   top_bar: {
     backgroundColor: "#2cd380",
     height: 90,
@@ -210,49 +108,6 @@ const styles = StyleSheet.create({
     right: 20,
     zIndex: 1,
   },
-  heading: {
-    fontSize: width * 0.05,
-    marginBottom: 20,
-    paddingHorizontal: 20,
-  },
-  menu_top: {
-    height: 90,
-    backgroundColor: "#2cd380",
-    color: "white",
-    textAlign: "center",
-    justifyContent: "center",
-  },
-  menu: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    height: height, // Take up full height of screen
-    width: width * 0.5, // Adjust width as needed
-    backgroundColor: "#f8f9fa", // Adjust background color here
-    borderColor: "#ccc",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  menuItem: {
-    marginBottom: 10,
-    padding: 10,
-    borderRadius: 4,
-    backgroundColor: "#f8f9fa",
-  },
-  menuItemText: {
-    fontSize: width * 0.04,
-    color: "black",
-  },
-  scrollViewContent: {
-    paddingTop: height * 0.08, // Adjust the paddingTop to accommodate the icons
-    flexGrow: 1,
-  },
   overlay: {
     position: "absolute",
     top: 0,
@@ -260,19 +115,6 @@ const styles = StyleSheet.create({
     width: width,
     height: height,
     zIndex: 0,
-  },
-  iconTextContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 10,
-    paddingHorizontal: 20,
-  },
-  iconText: {
-    marginLeft: 10,
-    fontSize: 18,
-  },
-  likeButton: {
-    marginLeft: "auto",
   },
 });
 
